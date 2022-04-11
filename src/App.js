8abe8130576e5b6887db145119b3acd4005befbb
bamboodia <crum82@gmail.com>
2022-04-11 18:09:48 +0100

    Framework is in place

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
 import "./App.css";
-import Header from './features/header/header.jsx'
+import Sidebar from "./features/sidebar/sidebar.jsx";
+import Header from "./features/header/header.jsx";
+import Card from "./components/card.jsx";
 
 function App() {
-	return <div className="App">
-    <div className="App-header">
-      <Header />
-    </div>
-  </div>;
+	return (
+		<div className="App">
+			<div className="sidebar-app">
+				<Sidebar />
+			</div>
+			<div className="main-app">
+				<Header />
+				<div className="cards-app">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./cardstyles.css";
+
+function Card() {
+	return <div className="card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid temporibus itaque dignissimos. Assumenda laborum voluptatibus exercitationem? In magni distinctio modi deleniti eum ut aliquid, earum magnam vel culpa, aperiam eaque.</div>;
+}
+
+export default Card;
